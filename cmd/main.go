7243f3ca82f0bffcadf8b02c6a42b1092a1a80bb
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-16 16:13:39 -0300

    .

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"go-jwt/pkg/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
